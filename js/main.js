c2ebda60c88d3200c39090109737bdebf8ad4f6b
anandhuajayakumar <anandhuaj.yakumar@gmail.com>
2017-03-08 10:59:31 +0530

    new

--- js/main.js
+++ js/main.js
@@ -31,13 +31,4 @@ jQuery(function($)
 		menuToggle();
 	});
 
-	$('.main-nav ul').onePageNav({
-		currentClass: 'active',
-	    changeHash: false,
-	    scrollSpeed: 900,
-	    scrollOffset: 0,
-	    scrollThreshold: 0.3,
-	    filter: ':not(.no-scroll)'
-	});
-
 });
